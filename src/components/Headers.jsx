0a1f48874288303491486d61feaa005c65fa9cc6
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-05 13:12:48 +0200

    add link to homepage

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Headers() {
   return (
     <header>
       <div className="flex justify-between items-center  bg-gray-100 p-2 rounded-md w-full">
         <div className="flex items-center gap-2">
           <img src="/vite.svg" alt="logo" />
-          <h1 className="text-2xl font-bold">Boolflix</h1>
+          <Link to="/" className="text-2xl font-bold">
+            Boolflix
+          </Link>
         </div>
-        <input className="bg-white rounded-md p-2" type="text" placeholder="Cerca un film" />
       </div>
       <div className="jumbotron w-full">
         <img className="w-full h-50 object-cover" src="../src/assets/img/jumbotron.png" alt="jumbotron" />
