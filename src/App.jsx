5e183da770d0bc5285216f70ebf647b33c363a6f
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-05 13:12:34 +0200

    add raouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import Headers from "./components/Headers";
 import Main from "./components/Main";
+import { DataProvider } from "./contexts/DataContext";
+import DefaultLayout from "./layout/DefaultLayout";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Movie from "./pages/Movie";
+import TvSeries from "./pages/TvSeries";
 
 function App() {
   return (
-    <>
-      <Headers />
-      <Main />
-    </>
+    <DataProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route index element={<Main />} />
+            <Route path="/movie" element={<Movie />} />
+            <Route path="/tv" element={<TvSeries />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </DataProvider>
   );
 }
 
