417e36521cf4c5e0f2ed4398ed90c085087fcee8
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-05 14:47:21 +0200

    add search in Header

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import { useData } from "../contexts/DataContext";
 
 export default function Headers() {
+  const { search, setSearch } = useData();
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <header>
       <div className="flex justify-between items-center  bg-gray-100 p-2 rounded-md w-full">
@@ -13,6 +20,12 @@ export default function Headers() {
       </div>
       <div className="jumbotron w-full">
         <img className="w-full h-50 object-cover" src="../src/assets/img/jumbotron.png" alt="jumbotron" />
+      </div>
+      <div className="flex items-center gap-2 p-2">
+        <input className="bg-white rounded-md p-2 border-2 border-gray-300" type="text" placeholder="Cerca un film" value={search} onChange={handleSearch} />
+        {/* <button className="bg-blue-500 text-white rounded-md p-2" onClick={handleSubmit}>
+          Cerca
+        </button> */}
       </div>
     </header>
   );
