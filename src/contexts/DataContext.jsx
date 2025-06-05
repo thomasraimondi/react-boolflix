24d1ed829c7d90145ca654e49435d133a152d3b6
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-05 14:48:14 +0200

    add api in context

--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -4,27 +4,39 @@ import axios from "axios";
 export const DataContext = createContext();
 
 const DataProvider = ({ children }) => {
-  const [data, setData] = useState([]);
+  const [dataMovies, setDataMovies] = useState([]);
+  const [dataTv, setDataTv] = useState([]);
+  const [topRatesMovies, setTopRatesMovies] = useState([]);
+  const [topRatesTv, setTopRatesTv] = useState([]);
+  const [search, setSearch] = useState("");
 
-  const fetchTopRatesData = (type) => {
-    const url = `https://api.themoviedb.org/3/${type}/top_rated?api_key=${import.meta.env.VITE_API_KEY}`;
+  const fetchTopRatesMovies = () => {
+    const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${import.meta.env.VITE_API_KEY}`;
     axios.get(url).then((res) => {
-      setData(res.data.results);
-      console.log(res.data.results);
+      setTopRatesMovies(res.data.results);
+    });
+  };
+  const fetchTopRatesTv = () => {
+    const url = `https://api.themoviedb.org/3/tv/top_rated?api_key=${import.meta.env.VITE_API_KEY}`;
+    axios.get(url).then((res) => {
+      setTopRatesTv(res.data.results);
     });
   };
 
-  const fetchSearchData = (type, search) => {
-    const url = `https://api.themoviedb.org/3/search/${type}?api_key=${import.meta.env.VITE_API_KEY}&query=${search}`;
+  const fetchSearchData = (search) => {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_API_KEY}&query=${search}`;
     axios
       .get(url)
       .then((res) => {
-        setData(res.data.results);
-        console.log(data);
+        setDataMovies(res.data.results);
       })
-      .catch((err) => console.log(err));
+      .then(() => {
+        axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${import.meta.env.VITE_API_KEY}&query=${search}`).then((res) => {
+          setDataTv(res.data.results);
+        });
+      });
   };
-  return <DataContext.Provider value={{ data, fetchSearchData, fetchTopRatesData }}>{children}</DataContext.Provider>;
+  return <DataContext.Provider value={{ dataMovies, dataTv, topRatesMovies, topRatesTv, fetchSearchData, fetchTopRatesMovies, fetchTopRatesTv, search, setSearch }}>{children}</DataContext.Provider>;
 };
 
 const useData = () => {
