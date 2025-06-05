ad14e8777846c60a9caa91900bb804a5ba65a52b
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-05 15:29:50 +0200

    header fixed

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -12,7 +12,7 @@ export default function Headers() {
 
   return (
     <header className="">
-      <div className="flex justify-between items-center  bg-black p-2 w-full fixed top-0 left-0 right-0 z-50 h-20">
+      <div className="flex justify-between items-center  bg-gray-900 p-2 w-full fixed top-0 left-0 right-0 z-50 h-20">
         <div className="flex items-center gap-5 w-full">
           <div className="flex items-center gap-2">
             <img src="/logo.jpeg" alt="logo" className="w-10 h-10" />
