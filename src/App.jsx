b9d1464b60b4ea3ceeb968dd3ef44a459832c9b3
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-05 17:46:47 +0200

    add new route in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Main from "./components/Main";
 import { DataProvider } from "./contexts/DataContext";
 import DefaultLayout from "./layout/DefaultLayout";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Search from "./pages/Search";
+import Tv from "./pages/tv";
+import Movie from "./pages/Movie";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<DefaultLayout />}>
             <Route index element={<Main />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/tv" element={<Tv />} />
+            <Route path="/movie" element={<Movie />} />
           </Route>
         </Routes>
       </BrowserRouter>
