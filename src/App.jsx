9fdbf0779d09bf06793a1c0f97e3fce0c9ca4b61
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-05 15:08:09 +0200

    add style in header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import Headers from "./components/Headers";
 import Main from "./components/Main";
 import { DataProvider } from "./contexts/DataContext";
 import DefaultLayout from "./layout/DefaultLayout";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Movie from "./pages/Movie";
-import TvSeries from "./pages/TvSeries";
 
 function App() {
   return (
--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { useData } from "../contexts/DataContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
 
 export default function Headers() {
   const { search, setSearch } = useData();
@@ -9,23 +11,36 @@ export default function Headers() {
   };
 
   return (
-    <header>
-      <div className="flex justify-between items-center  bg-gray-100 p-2 rounded-md w-full">
-        <div className="flex items-center gap-2">
-          <img src="/vite.svg" alt="logo" />
-          <Link to="/" className="text-2xl font-bold">
-            Boolflix
+    <header className="">
+      <div className="flex justify-between items-center  bg-black p-2 w-full fixed top-0 left-0 right-0 z-50 h-20">
+        <div className="flex items-center gap-5 w-full">
+          <div className="flex items-center gap-2">
+            <img src="/logo.jpeg" alt="logo" className="w-10 h-10" />
+            <Link to="/" className="text-2xl font-bold text-red-500">
+              Boolflix
+            </Link>
+          </div>
+          <div className="flex items-center gap-5 text-white">
+            <Link to="/">Home</Link>
+            <Link to="/">Serie Tv</Link>
+            <Link to="/">Film</Link>
+            <Link to="/">Giochi</Link>
+            <Link to="/">Nuovi e Popolari</Link>
+            <Link to="/">La mia lista</Link>
+            <Link to="/">Sfoglia per lingua</Link>
+          </div>
+        </div>
+        <div className="flex items-center gap-5 text-white pr-5">
+          <div className="flex items-center gap-2 p-2">
+            <input className=" p-2 border-2 border-gray-300" type="text" placeholder="Cerca un film" value={search} onChange={handleSearch} />
+          </div>
+          <Link to="/">
+            <FontAwesomeIcon icon={faUser} />
           </Link>
         </div>
       </div>
-      <div className="jumbotron w-full">
+      <div className="jumbotron w-full mt-20">
         <img className="w-full h-50 object-cover" src="../src/assets/img/jumbotron.png" alt="jumbotron" />
-      </div>
-      <div className="flex items-center gap-2 p-2">
-        <input className="bg-white rounded-md p-2 border-2 border-gray-300" type="text" placeholder="Cerca un film" value={search} onChange={handleSearch} />
-        {/* <button className="bg-blue-500 text-white rounded-md p-2" onClick={handleSubmit}>
-          Cerca
-        </button> */}
       </div>
     </header>
   );
