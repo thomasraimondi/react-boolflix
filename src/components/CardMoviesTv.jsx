620846f0e9e55c7fd9af5dfc2207dbbbc7590203
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-06 06:57:58 +0200

    change overlay information

--- src/components/CardMoviesTv.jsx
+++ src/components/CardMoviesTv.jsx
@@ -11,19 +11,15 @@ export default function CardMoviesTv({ item }) {
   return (
     <div className="relative flex flex-col gap-2 w-48 rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition-all duration-300 text-white group bg-gray-800">
       {item.poster_path ? (
-        <img className="w-full h-40 object-cover" src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={title} />
+        <img className="w-full h-70 object-cover" src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={title} />
       ) : (
-        <img className="w-full h-40 object-cover" src="/logo.jpeg" alt={title} />
+        <img className="w-full h-70 object-cover" src="/logo.jpeg" alt={title} />
       )}
       {/* Overlay per overview in hover */}
-      {/* <div className="absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 flex items-center justify-center p-4 transition-opacity duration-300 text-sm text-gray-200 z-10">
-        <span>{overview.length > 180 ? overview.slice(0, 180) + "..." : overview}</span>
-      </div> */}
-      <div className="flex flex-col gap-1 p-3 z-20">
-        <span className="font-bold text-lg truncate" title={title}>
-          {title}
-        </span>
-        <span className="text-xs text-gray-400">{year}</span>
+      <div className="absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 flex flex-col p-4 transition-opacity duration-300 text-sm text-gray-200 z-10">
+        <span className="text-white">{title}</span>
+        <span className="text-white">Overview:</span>
+        <span>{overview.length > 120 ? overview.slice(0, 120) + "..." : overview}</span>
         <div className="flex items-center gap-2 mt-1">
           <span className="flex">
             {Array.from({ length: Math.round(item.vote_average / 2) }, (_, i) => (
@@ -40,6 +36,12 @@ export default function CardMoviesTv({ item }) {
           <span className="text-sm font-semibold ml-1">{vote}</span>
         </div>
       </div>
+      {/* <div className="flex flex-col gap-1 p-3">
+        <span className="font-bold text-lg truncate" title={title}>
+          {title}
+        </span>
+        <span className="text-xs text-gray-400">{year}</span>
+      </div> */}
     </div>
   );
 }
