591ba4467089cf712f3f654ccc224ec832733048
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-06 09:18:05 +0200

    manage disable butto prev and next

--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,28 +1,33 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import CardMoviesTv from "./CardMoviesTv";
+import { useRef, useEffect } from "react";
 
 export default function ListCard({ searchedData, page, setPage }) {
   return (
     <div className="flex gap-4 p-2 justify-center">
       <div className="flex gap-2 py-5">
-        {page > 1 && (
-          <button className="text-white px-4 py-2 rounded-md hover:scale-105 transition-all duration-300 text-5xl" onClick={() => setPage(page - 1)}>
-            <FontAwesomeIcon icon={faChevronLeft} />
-          </button>
-        )}
+        <button
+          className="text-white px-4 py-2 rounded-md hover:scale-105 transition-all duration-300 text-5xl disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:scale-100"
+          onClick={() => setPage(page - 1)}
+          disabled={page === 1}
+        >
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
       </div>
-      <div className="flex flex-wrap gap-4 p-2 justify-center">
+      <div className="flex gap-4 p-2 overflow-x-auto whitespace-nowrap scrollbar-hide items-center">
         {searchedData.map((item) => (
           <CardMoviesTv key={item.id} item={item} />
         ))}
       </div>
       <div className="flex gap-2 py-5">
-        {searchedData.length === 20 && (
-          <button className="text-white px-4 py-2 rounded-md hover:scale-105 transition-all duration-300 text-5xl" onClick={() => setPage(page + 1)}>
-            <FontAwesomeIcon icon={faChevronRight} />
-          </button>
-        )}
+        <button
+          className="text-white px-4 py-2 rounded-md hover:scale-105 transition-all duration-300 text-5xl disabled:opacity-50 disabled:cursor-not-allowed"
+          onClick={() => setPage(page + 1)}
+          disabled={searchedData.length < 20}
+        >
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
       </div>
     </div>
   );
